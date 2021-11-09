3aaff56128539c67ef083ef5b95f233a9546dd63
Natalia D <paninebaluy@gmail.com>
2021-11-09 21:09:31 +0300

    add an article condition

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ const dogs = require('./dogs'); // ES5
 
 const getRandomArrElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-const mood = getRandomArrElement(dogs.moods.negative);
-const breed = getRandomArrElement(dogs.breeds);
-const action = getRandomArrElement(dogs.actions);
-const verb = getRandomArrElement(dogs.verbs.negative);
-const message = getRandomArrElement(dogs.messages);
+
 
 const getMessage  = () => {
-    return `A ${mood} ${breed} ${action} and ${verb}: "${message}"`;
+    const mood = getRandomArrElement(dogs.moods.negative);
+    const article = 'aoiue'.includes(mood[0]) ? 'An' : 'A'; 
+    const breed = getRandomArrElement(dogs.breeds);
+    const action = getRandomArrElement(dogs.actions);
+    const verb = getRandomArrElement(dogs.verbs.negative);
+    const message = getRandomArrElement(dogs.messages);
+
+    return `${article} ${mood} ${breed} ${action} and ${verb}: "${message}"`;
 }
 
+console.log(getMessage());
+console.log(getMessage());
+console.log(getMessage());
+console.log(getMessage());
+console.log(getMessage());
+console.log(getMessage());
+console.log(getMessage());
 console.log(getMessage());

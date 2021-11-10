dc2f00bc68b975689390a7d7d344dfdf4b5b316f
Natalia D <paninebaluy@gmail.com>
2021-11-11 02:30:26 +0300

    fix responsive layouts

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ largeDogBtn.addEventListener('click', getLargeSpeech);
 largeDogBtn.addEventListener('touchend', getLargeSpeech);
 resetBtn.addEventListener('click', resetBoxes);
 resetBtn.addEventListener('touchend', resetBoxes);
-imgArray.forEach(img, () => {
+imgArray.forEach(img => {
     img.addEventListener('touchstart', (evt) => {
         evt.preventDefault();
         evt.closest('.card'.style.transform = "translate(20px, 20px) scale(1.1)");
